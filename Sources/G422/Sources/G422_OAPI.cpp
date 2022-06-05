464692428ccdf6950f4f8208396d067b6babf48c
Gondos <thegondos@protonmail.com>
2022-06-05 23:50:12 +0200

    Use new module API

--- Sources/G422/Sources/G422_OAPI.cpp
+++ Sources/G422/Sources/G422_OAPI.cpp
@@ -3,9 +3,9 @@
 #include "../Headers/G422.h"
 #include "../Headers/G422_DVC.h"
 
-DLLCLBK void InitModule(oapi::DynamicModule *mod) { }
+DLLCLBK void InitModule(MODULEHANDLE mod) { }
 
-DLLCLBK void ExitModule(oapi::DynamicModule *mod) { }
+DLLCLBK void ExitModule(MODULEHANDLE mod) { }
 
 DLLCLBK VESSEL* ovcInit(OBJHANDLE vessel_hndl, int fltModel) { return new G422(vessel_hndl, fltModel); }
 
